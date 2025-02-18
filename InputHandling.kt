86651b72c5e61659ca81775d75655d56ac173752
Payal Jain <166532121+Payal-jain123@users.noreply.github.com>
2025-02-18 22:18:22 +0530

    It is used to handle the integer

---
+++ InputHandling.kt
@@ -0,0 +1,11 @@
+package com.example.rockpaperscissors
+
+fun main(){
+    println("Enter a number : ")
+    val inputString= readln()
+    val inputNumber=inputString.toInt()
+    val multiplier=5
+    val answer=5+inputNumber
+    println("Result is :" +answer)
+
+}
